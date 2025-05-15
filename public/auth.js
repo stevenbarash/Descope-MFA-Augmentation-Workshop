bfe10e18042d9800413af10a5432c10af7789d69
mrunankpawar <mrunank.pawar@descope.com>
2025-05-15 09:29:13 -0700

    added url for testing

--- public/auth.js
+++ public/auth.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = process.env.FRONTEND_URL + "/api"; // Replace with your API base URL
+const API_BASE_URL = "https://homegrown-auth-server.preview.descope.org/api";
 
 // DOM Elements
 const loginForm = document.getElementById('login-form');
